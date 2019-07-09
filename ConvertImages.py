8d44faa9d82ba46a028975dbc0ee29f725b75c99
Jacob Johnson <johnsonj118@gmail.com>
2019-07-09 17:43:45 +0000

    Full working code but images do not match masks

--- ConvertImages.py
+++ ConvertImages.py
@@ -2,6 +2,7 @@ import csv
 import glob
 import os
 import numpy as np
+from tqdm import tqdm
 
 import cv2
 import pydicom as dcm
@@ -14,6 +15,8 @@ train_outpath = '/data/Kaggle/train-png'
 train_mask_path = '/data/Kaggle/train-mask'
 if not os.path.exists(train_outpath):
     os.mkdir(train_outpath)
+if not os.path.exists(train_mask_path):
+    os.mkdir(train_mask_path)
 csv_file = 'train-rle.csv'
 
 all_files = [f for f in glob.glob(
@@ -26,7 +29,8 @@ def splitfile(file):
 
 
 # Convert all dicoms to png
-for cur_file in all_files:
+print('Converting DICOMs to PNG...')
+for cur_file in tqdm(all_files):
     ds = dcm.dcmread(cur_file)
     cur_name = splitfile(cur_file) + '.png'
     cur_outpath = os.path.join(train_outpath, cur_name)
@@ -44,12 +48,14 @@ all_rle = [d[1] for d in data]
 unq_file_ids = list(set(all_file_ids))
 
 # Generate masks to PNG
-for cur_id in unq_file_ids:
+print('Converting masks to PNG....')
+for cur_id in tqdm(unq_file_ids):
     # find matches for current file id
     matches = [i for i,d in enumerate(all_file_ids) if d == cur_id]
-    if all_rle[matches[0]] == '-1':
+    if len(all_rle[matches[0]]) < 4:
         # no annotation for this image
-        continue
+        # make blank mask
+        mask = np.zeros((1024,1024))
     if len(matches) > 1:
         # combine multiple annotations into one mask
         cur_rle = [all_rle[m] for m in matches]
@@ -58,8 +64,19 @@ for cur_id in unq_file_ids:
     else:
         # convert single annotation to mask
         cur_rle = all_rle[matches[0]]
-        mask = rle2mask(cur_rle,1024,1024)
+        try:
+            mask = rle2mask(cur_rle,1024,1024)
+        except IndexError as e:
+            print(cur_id)
+            raise IndexError(e)
     # write mask tp png
     cur_outpath = os.path.join(train_mask_path,cur_id+'.png')
     cv2.imwrite(cur_outpath,mask)
+
+print('Done')
+
+test_id = '1.2.276.0.7230010.3.1.4.8323329.3604.1517875178.653360'
+img = Image.open(os.path.join(train_outpath,test_id+'.png'))
+mask_img = Image.open(os.path.join(train_mask_path,test_id+'.png'))
+showmask(np.array(img),np.array(mask_img)/255)
 
