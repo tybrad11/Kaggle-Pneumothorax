382616d7a9c8b160d1842cd636583f26612a6ee4
Johnson Jacob M <johnsonj118@gmail.com>
2019-07-19 11:00:33 -0500

    updated but still not usable

--- UseKaggleClass.py
+++ UseKaggleClass.py
@@ -1,8 +1,6 @@
 from KaggleModelClass import PneumothoraxModel
 
-KaggleObject = PneumothoraxModel(image_path = '/data/Kaggle/train-png',
-                                 mask_path = '/data/Kaggle/train-mask',
-                                 test_path = '/data/Kaggle/test-png',
+KaggleObject = PneumothoraxModel(test_path = '/data/Kaggle/test-png',
                                  dims = (1024,1024),
                                  batch_size = 8,
                                  val_split = .15,
