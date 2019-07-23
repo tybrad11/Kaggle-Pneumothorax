b2e36c2fc0cf7ee17dc14678a0e5389717506ad8
Johnson Jacob M <johnsonj118@gmail.com>
2019-07-23 09:46:12 -0500

    added training and full training steps

--- TrainClassificationModel.py
+++ TrainClassificationModel.py
@@ -17,7 +17,7 @@ from Datagen import PngClassDataGenerator, PngDataGenerator
 from HelperFunctions import (RenameWeights, get_class_datagen, get_seg_datagen,
                              get_train_params, get_val_params)
 from Losses import dice_coef_loss
-from Models import BlockModel2D, BlockModel_Classifier, ConvertEncoderToCED
+from Models import Inception_model
 
 os.environ['HDF5_USE_FILE_LOCKING'] = 'false'
 
@@ -45,7 +45,7 @@ train_pos_datapath = '/data/Kaggle/pos-norm-png'
 train_neg_datapath = '/data/Kaggle/neg-norm-png'
 
 pretrain_weights_filepath = 'Best_pretrain_class_weights.h5'
-train_weight_filepath = 'Best_Kaggle_Classification_Weights_{}.h5'
+train_weights_filepath = 'Best_Kaggle_Classification_Weights_{}.h5'
 
 # pre-train parameters
 pre_im_dims = (512, 512)
@@ -60,9 +60,11 @@ im_dims = (512, 512)
 n_channels = 1
 batch_size = 4
 learnRate = 1e-4
+filt_nums = 16
+num_blocks = 5
 val_split = .2
-epochs = 2  # epochs before and after unfreezing weights
-full_epochs = 20 # epochs trained on 1024 data
+epochs = 1
+full_epochs = 1  # epochs trained on 1024 data
 multi_process = False
 
 # datagen params
@@ -87,11 +89,11 @@ pre_train_gen, pre_val_gen, class_weights = get_class_datagen(
     pre_train_datapath, pre_train_negative_datapath, pre_train_params, pre_val_params, pre_val_split)
 
 # Create model
-pre_model = BlockModel_Classifier(input_shape=pre_im_dims+(pre_n_channels,),
-                                  filt_num=filt_nums, numBlocks=num_blocks)
+model = Inception_model(input_shape=pre_im_dims+(pre_n_channels,))
 
 # Compile model
-pre_model.compile(Adam(), loss='binary_crossentropy', metrics=['accuracy'])
+model.compile(Adam(lr=learnRate), loss='binary_crossentropy',
+              metrics=['accuracy'])
 
 # Create callbacks
 cb_check = ModelCheckpoint(pretrain_weights_filepath, monitor='val_loss',
@@ -102,19 +104,19 @@ print('----- Starting pre-training -----')
 print('---------------------------------')
 
 # Train model
-pre_history = pre_model.fit_generator(generator=pre_train_gen,
-                                      epochs=pre_epochs, use_multiprocessing=pre_multi_process,
-                                      workers=8, verbose=1, callbacks=[cb_check],
-                                      class_weight=class_weights,
-                                      validation_data=pre_val_gen)
+pre_history = model.fit_generator(generator=pre_train_gen,
+                                  epochs=pre_epochs, use_multiprocessing=pre_multi_process,
+                                  workers=8, verbose=1, callbacks=[cb_check],
+                                  class_weight=class_weights,
+                                  validation_data=pre_val_gen)
 
 # Load best weights
-pre_model.load_weights(pretrain_weights_filepath)
+model.load_weights(pretrain_weights_filepath)
 
 # Calculate confusion matrix
 print('Calculating classification confusion matrix...')
 pre_val_gen.shuffle = False
-preds = pre_model.predict_generator(pre_val_gen, verbose=1)
+preds = model.predict_generator(pre_val_gen, verbose=1)
 labels = [pre_val_gen.labels[f] for f in pre_val_gen.list_IDs]
 y_pred = np.rint(preds)
 totalNum = len(y_pred)
@@ -124,6 +126,116 @@ tn, fp, fn, tp = confusion_matrix(y_true, y_pred).ravel()
 print('--------------------------------------')
 print('Classification Results on pre-training')
 print('--------------------------------------')
+print('True positives: {}'.format(tp))
+print('True negatives: {}'.format(tn))
+print('False positives: {}'.format(fp))
+print('False negatives: {}'.format(fn))
+print('% Positive: {:.02f}'.format(100*(tp+fp)/totalNum))
+print('% Negative: {:.02f}'.format(100*(tn+fn)/totalNum))
+print('% Accuracy: {:.02f}'.format(100*(tp+tn)/totalNum))
+print('% Sensitivity: {:.02f}'.format(100*(tp)/(tp+fn)))
+print('% Specificity: {:.02f}'.format(100*(tn)/(tn+fp)))
+print('-----------------------')
+
+# %% ~~~~~~~~~~~~~~~~~~~~~~~~~~
+# ~~~~~~~ 512 Training~~~~~~~~~
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+print('---------------------------------')
+print('---- Setting up 512 training ----')
+print('---------------------------------')
+
+# Get datagens for training
+train_gen, val_gen, class_weights = get_class_datagen(
+    train_pos_datapath, train_neg_datapath, train_params, val_params, val_split)
+
+# Create callbacks
+cur_weights_path = train_weights_filepath.format('512train')
+cb_check = ModelCheckpoint(cur_weights_path, monitor='val_loss', verbose=1,
+                           save_best_only=True, save_weights_only=True, mode='auto', period=1)
+
+print('---------------------------------')
+print('----- Starting 512 training -----')
+print('---------------------------------')
+
+# Train model
+history = model.fit_generator(generator=train_gen,
+                              epochs=epochs, use_multiprocessing=multi_process,
+                              workers=8, verbose=1, callbacks=[cb_check],
+                              class_weight=class_weights,
+                              validation_data=val_gen)
+
+# Load best weights
+model.load_weights(cur_weights_path)
+
+# Calculate confusion matrix
+print('Calculating classification confusion matrix...')
+val_gen.shuffle = False
+preds = model.predict_generator(val_gen, verbose=1)
+labels = [val_gen.labels[f] for f in val_gen.list_IDs]
+y_pred = np.rint(preds)
+totalNum = len(y_pred)
+y_true = np.rint(labels)[:totalNum]
+tn, fp, fn, tp = confusion_matrix(y_true, y_pred).ravel()
+
+print('--------------------------------------')
+print('Classification Results on 512 training')
+print('--------------------------------------')
+print('True positives: {}'.format(tp))
+print('True negatives: {}'.format(tn))
+print('False positives: {}'.format(fp))
+print('False negatives: {}'.format(fn))
+print('% Positive: {:.02f}'.format(100*(tp+fp)/totalNum))
+print('% Negative: {:.02f}'.format(100*(tn+fn)/totalNum))
+print('% Accuracy: {:.02f}'.format(100*(tp+tn)/totalNum))
+print('% Sensitivity: {:.02f}'.format(100*(tp)/(tp+fn)))
+print('% Specificity: {:.02f}'.format(100*(tn)/(tn+fp)))
+print('-----------------------')
+
+# %% ~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# ~~~~~~~ 1024 Training~~~~~~~~~
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+print('----------------------------------')
+print('---- Setting up 1024 training ----')
+print('----------------------------------')
+
+# Get datagens for training
+full_train_gen, full_val_gen, class_weights = get_class_datagen(
+    train_pos_datapath, train_neg_datapath, full_train_params, full_val_params, val_split)
+
+# Create callbacks
+cur_weights_path = train_weights_filepath.format('1024train')
+cb_check = ModelCheckpoint(cur_weights_path, monitor='val_loss', verbose=1,
+                           save_best_only=True, save_weights_only=True, mode='auto', period=1)
+
+print('----------------------------------')
+print('----- Starting 1024 training -----')
+print('----------------------------------')
+
+# Train model
+history = model.fit_generator(generator=full_train_gen,
+                              epochs=epochs, use_multiprocessing=multi_process,
+                              workers=8, verbose=1, callbacks=[cb_check],
+                              class_weight=class_weights,
+                              validation_data=full_val_gen)
+
+# Load best weights
+model.load_weights(cur_weights_path)
+
+# Calculate confusion matrix
+print('Calculating classification confusion matrix...')
+val_gen.shuffle = False
+preds = model.predict_generator(val_gen, verbose=1)
+labels = [val_gen.labels[f] for f in val_gen.list_IDs]
+y_pred = np.rint(preds)
+totalNum = len(y_pred)
+y_true = np.rint(labels)[:totalNum]
+tn, fp, fn, tp = confusion_matrix(y_true, y_pred).ravel()
+
+print('---------------------------------------')
+print('Classification Results on 1024 training')
+print('---------------------------------------')
 print('True positives: {}'.format(tp))
 print('True negatives: {}'.format(tn))
 print('False positives: {}'.format(fp))
