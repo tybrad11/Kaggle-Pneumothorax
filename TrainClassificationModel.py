71ad64fc2259a609c2d920d169dec2eaab701af9
Johnson Jacob M <johnsonj118@gmail.com>
2019-08-05 09:54:13 -0500

    Changed paths and added skip pretrain option

--- TrainClassificationModel.py
+++ TrainClassificationModel.py
@@ -46,19 +46,20 @@ pre_train_negative_datapath = '/data/Kaggle/nih-chest-dataset/images_resampled_s
 
 # train_pos_datapath = '/data/Kaggle/pos-norm-png'
 # train_neg_datapath = '/data/Kaggle/neg-norm-png'
-pos_img_path = '/data/Kaggle/pos-filt-png'
-
+train_pos_datapath = '/data/Kaggle/pos-filt-png'
+train_neg_datapath = '/data/Kaggle/neg-filt-png'
 
 pretrain_weights_filepath = 'Best_pretrain_class_weights.h5'
-train_weights_filepath = 'Best_Kaggle_Classification_Weights_{}_v2.h5'
+train_weights_filepath = 'Best_Kaggle_Classification_Weights_{}_v4.h5'
 
 # pre-train parameters
 pre_im_dims = (512, 512)
 pre_n_channels = 1
 pre_batch_size = 16
 pre_val_split = .15
-pre_epochs = 20
+pre_epochs = 10
 pre_multi_process = False
+skip_pretrain = True
 
 # train parameters
 im_dims = (512, 512)
@@ -67,9 +68,9 @@ batch_size = 4
 learnRate = 1e-4
 filt_nums = 16
 num_blocks = 5
-val_split = .2
-epochs = 20
-full_epochs = 20  # epochs trained on 1024 data
+val_split = .15
+epochs = 10
+full_epochs = 30  # epochs trained on 1024 data
 multi_process = False
 
 # datagen params
@@ -85,62 +86,74 @@ full_val_params = get_val_params(2, (1024, 1024), 1)
 # ~~~~Pre-training~~~~~~
 # ~~~~~~~~~~~~~~~~~~~~~~~
 
-print('---------------------------------')
-print('---- Setting up pre-training ----')
-print('---------------------------------')
+if not skip_pretrain:
 
-# Get datagens for pre-training
-pre_train_gen, pre_val_gen, class_weights = get_class_datagen(
-    pre_train_datapath, pre_train_negative_datapath, pre_train_params, pre_val_params, pre_val_split)
+    print('---------------------------------')
+    print('---- Setting up pre-training ----')
+    print('---------------------------------')
 
-# Create model
-model = Inception_model(input_shape=pre_im_dims+(pre_n_channels,))
+    # Get datagens for pre-training
+    pre_train_gen, pre_val_gen, class_weights = get_class_datagen(
+        pre_train_datapath, pre_train_negative_datapath, pre_train_params, pre_val_params, pre_val_split)
 
-# Compile model
-model.compile(Adam(lr=learnRate), loss='binary_crossentropy',
-              metrics=['accuracy'])
+    # Create model
+    model = Inception_model(input_shape=pre_im_dims+(pre_n_channels,))
 
-# Create callbacks
-cb_check = ModelCheckpoint(pretrain_weights_filepath, monitor='val_loss',
-                           verbose=1, save_best_only=True, save_weights_only=True, mode='auto', period=1)
+    # Compile model
+    model.compile(Adam(lr=learnRate), loss='binary_crossentropy',
+                metrics=['accuracy'])
 
-print('---------------------------------')
-print('----- Starting pre-training -----')
-print('---------------------------------')
+    # Create callbacks
+    cb_check = ModelCheckpoint(pretrain_weights_filepath, monitor='val_loss',
+                            verbose=1, save_best_only=True, save_weights_only=True, mode='auto', period=1)
 
-# Train model
-pre_history = model.fit_generator(generator=pre_train_gen,
-                                  epochs=pre_epochs, use_multiprocessing=pre_multi_process,
-                                  workers=8, verbose=1, callbacks=[cb_check],
-                                  class_weight=class_weights,
-                                  validation_data=pre_val_gen)
+    print('---------------------------------')
+    print('----- Starting pre-training -----')
+    print('---------------------------------')
 
-# Load best weights
-model.load_weights(pretrain_weights_filepath)
+    # Train model
+    pre_history = model.fit_generator(generator=pre_train_gen,
+                                    epochs=pre_epochs, use_multiprocessing=pre_multi_process,
+                                    workers=8, verbose=1, callbacks=[cb_check],
+                                    class_weight=class_weights,
+                                    validation_data=pre_val_gen)
 
-# Calculate confusion matrix
-print('Calculating classification confusion matrix...')
-pre_val_gen.shuffle = False
-preds = model.predict_generator(pre_val_gen, verbose=1)
-labels = [pre_val_gen.labels[f] for f in pre_val_gen.list_IDs]
-y_pred = np.rint(preds)
-totalNum = len(y_pred)
-y_true = np.rint(labels)[:totalNum]
-tn, fp, fn, tp = confusion_matrix(y_true, y_pred).ravel()
+    # Load best weights
+    model.load_weights(pretrain_weights_filepath)
 
-print('--------------------------------------')
-print('Classification Results on pre-training')
-print('--------------------------------------')
-print('True positives: {}'.format(tp))
-print('True negatives: {}'.format(tn))
-print('False positives: {}'.format(fp))
-print('False negatives: {}'.format(fn))
-print('% Positive: {:.02f}'.format(100*(tp+fp)/totalNum))
-print('% Negative: {:.02f}'.format(100*(tn+fn)/totalNum))
-print('% Accuracy: {:.02f}'.format(100*(tp+tn)/totalNum))
-print('% Sensitivity: {:.02f}'.format(100*(tp)/(tp+fn)))
-print('% Specificity: {:.02f}'.format(100*(tn)/(tn+fp)))
-print('-----------------------')
+    # Calculate confusion matrix
+    print('Calculating classification confusion matrix...')
+    pre_val_gen.shuffle = False
+    preds = model.predict_generator(pre_val_gen, verbose=1)
+    labels = [pre_val_gen.labels[f] for f in pre_val_gen.list_IDs]
+    y_pred = np.rint(preds)
+    totalNum = len(y_pred)
+    y_true = np.rint(labels)[:totalNum]
+    tn, fp, fn, tp = confusion_matrix(y_true, y_pred).ravel()
+
+    print('--------------------------------------')
+    print('Classification Results on pre-training')
+    print('--------------------------------------')
+    print('True positives: {}'.format(tp))
+    print('True negatives: {}'.format(tn))
+    print('False positives: {}'.format(fp))
+    print('False negatives: {}'.format(fn))
+    print('% Positive: {:.02f}'.format(100*(tp+fp)/totalNum))
+    print('% Negative: {:.02f}'.format(100*(tn+fn)/totalNum))
+    print('% Accuracy: {:.02f}'.format(100*(tp+tn)/totalNum))
+    print('% Sensitivity: {:.02f}'.format(100*(tp)/(tp+fn)))
+    print('% Specificity: {:.02f}'.format(100*(tn)/(tn+fp)))
+    print('-----------------------')
+
+else:
+    # skip pretraining, load weights and go to regular training
+    print('Skipping pre-training, setting up model')
+    # Create model
+    model = Inception_model(input_shape=pre_im_dims+(pre_n_channels,))
+    # Compile model
+    model.compile(Adam(lr=learnRate), loss='binary_crossentropy',
+                metrics=['accuracy'])
+    model.load_weights(pretrain_weights_filepath)
 
 # %% ~~~~~~~~~~~~~~~~~~~~~~~~~~
 # ~~~~~~~ 512 Training~~~~~~~~~
